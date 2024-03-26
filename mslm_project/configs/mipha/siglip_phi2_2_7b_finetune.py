925fab9812e13fe1783b6d5268121dd0c5a974e2
huanghaian <huanghaian@sensetime.com>
2024-03-26 17:30:01 +0800

    add textvqa

--- mslm_project/configs/mipha/siglip_phi2_2_7b_finetune.py
+++ mslm_project/configs/mipha/siglip_phi2_2_7b_finetune.py
@@ -206,7 +206,7 @@ randomness = dict(seed=None, deterministic=False)
 log_processor = dict(by_epoch=False)
 
 # 评估时候只会读取 model + eval_dataset 参数，其他参数忽略
-from mslm_project.evaluation.dataset import MultipleChoiceLLaVADataset, MMELLaVADataset
+from mslm_project.evaluation.dataset import MultipleChoiceLLaVADataset, MMELLaVADataset, TextVQALLaVADataset
 
 eval_dataset = [
     # MMBench_DEV_EN/SEEDBench_IMG/MMMU/scienceqa
@@ -217,13 +217,13 @@ eval_dataset = [
     #     tokenizer=tokenizer,
     #     image_processor=image_processor,
     #     pad_image_to_square=True),
-    dict(
-        type=MultipleChoiceLLaVADataset,
-        data_file='/mnt/petrelfs/huanghaian/code/xtuner/LMUData/SEEDBench_IMG.tsv',
-        prompt_template=PROMPT_TEMPLATE.vicuna,
-        tokenizer=tokenizer,
-        image_processor=image_processor,
-        pad_image_to_square=True),
+    # dict(
+    #     type=MultipleChoiceLLaVADataset,
+    #     data_file='/mnt/petrelfs/huanghaian/code/xtuner/LMUData/SEEDBench_IMG.tsv',
+    #     prompt_template=PROMPT_TEMPLATE.vicuna,
+    #     tokenizer=tokenizer,
+    #     image_processor=image_processor,
+    #     pad_image_to_square=True),
     # dict(
     #     type=MMELLaVADataset,
     #     data_file='/mnt/petrelfs/huanghaian/code/xtuner/LMUData/MME.tsv',
@@ -231,4 +231,13 @@ eval_dataset = [
     #     tokenizer=tokenizer,
     #     image_processor=image_processor,
     #     pad_image_to_square=True),
+    dict(
+        type=TextVQALLaVADataset,
+        data_file='/mnt/petrelfs/share_data/huanghaian/orig_llava_eval/textvqa/llava_textvqa_val_v051_ocr.jsonl',
+        ann_file='/mnt/petrelfs/share_data/huanghaian/text_vqa/TextVQA_0.5.1_val.json',
+        image_folder='/mnt/petrelfs/share_data/huanghaian/text_vqa/train_images',
+        prompt_template=PROMPT_TEMPLATE.vicuna,
+        tokenizer=tokenizer,
+        image_processor=image_processor,
+        pad_image_to_square=True),
 ]
--- mslm_project/evaluation/dataset/__init__.py
+++ mslm_project/evaluation/dataset/__init__.py
@@ -1,4 +1,5 @@
 from .multiple_choice_llava import MultipleChoiceLLaVADataset
 from .mme_llava import MMELLaVADataset
+from .textvqa_llava import TextVQALLaVADataset
 
-__all__ = ['MultipleChoiceLLaVADataset', 'MMELLaVADataset']
+__all__ = ['MultipleChoiceLLaVADataset', 'MMELLaVADataset', 'TextVQALLaVADataset']
