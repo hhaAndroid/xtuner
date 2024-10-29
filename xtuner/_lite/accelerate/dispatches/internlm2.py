ce74afd3c83c12686578b1a3c4436f394e21cf5a
huanghaian <huanghaian@sensetime.com>
2024-10-29 13:03:16 +0800

    支持 sp cp

--- xtuner/_lite/accelerate/dispatches/internlm2.py
+++ xtuner/_lite/accelerate/dispatches/internlm2.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from typing import Optional, Tuple
 
+import os
 import torch
 from einops import rearrange
 from mmengine import MessageHub
@@ -8,6 +9,8 @@ from transformers.cache_utils import StaticCache
 
 from ._attention import SUPPORT_FLASH2, flash_attn_wo_mask, varlen_flash_attn
 import torch.nn.functional as F
+from xtuner._lite.yunchang import llama3_varlen_attention_sp_ulysses_ring
+from xtuner._lite.parallel.setup import get_ring_group, get_ring_world_size, get_sp_world_size, get_ulysess_group
 
 
 class InternLM2RotaryEmbedding(torch.nn.Module):
@@ -227,9 +230,29 @@ def _internlm2_varlen_attn_forward(
     assert SUPPORT_FLASH2
     cumulative_lengths = attn_context.get_info('cumulative_lengths')
     if cumulative_lengths is not None and SUPPORT_FLASH2 and bsz == 1:
-        max_seqlen = attn_context.get_info('max_seqlen')
-        attn_output = varlen_flash_attn(query_states, key_states, value_states,
-                                        cumulative_lengths, max_seqlen)
+        # 仅仅用于测试该分支下 sp ulyess 是否正确
+        force_to_new_sp = os.environ.get('FORCE_TO_NEW_SP')
+        if get_ring_world_size() > 1 or force_to_new_sp:
+            # 只有开启了 ring 情况下才运行，如果只是普通 sp，则依然运行原先逻辑
+            assert cumulative_lengths[-1] % get_sp_world_size() == 0, f'==={cumulative_lengths[-1]}===='
+            q_unpad, k_unpad, v_unpad = query_states.flatten(0, 1), key_states.flatten(
+                0, 1), value_states.flatten(0, 1)
+            attn_output = llama3_varlen_attention_sp_ulysses_ring(
+                q_unpad,
+                k_unpad,
+                v_unpad,
+                cumulative_lengths,
+                ulysses_pg=get_ulysess_group(),
+                ring_pg=get_ring_group(),
+                causal=True,
+                # 如果想更省显存，可以设置为 1。-1 表示不切分
+                heads_k_stride=-1
+            )
+            attn_output = attn_output.unsqueeze(0)
+        else:
+            max_seqlen = attn_context.get_info('max_seqlen')
+            attn_output = varlen_flash_attn(query_states, key_states, value_states,
+                                            cumulative_lengths, max_seqlen)
     else:
         attn_output = flash_attn_wo_mask(
             query_states,
