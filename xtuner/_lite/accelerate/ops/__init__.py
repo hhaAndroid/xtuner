185eca5229b72815815e054a6e211cf98ffb3237
huanghaian <huanghaian@sensetime.com>
2025-02-26 17:34:07 +0800

    add groupgemm

--- xtuner/_lite/accelerate/ops/__init__.py
+++ xtuner/_lite/accelerate/ops/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .moe_permute import GROUPED_GEMM_INSTALLED, permute_func, unpermute_func
+from .grouped_gemm import gmm
 
-__all__ = ["GROUPED_GEMM_INSTALLED", "permute_func", "unpermute_func"]
+__all__ = ["GROUPED_GEMM_INSTALLED", "permute_func", "unpermute_func", 'gmm']
