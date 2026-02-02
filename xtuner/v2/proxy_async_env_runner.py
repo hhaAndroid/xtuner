ec56f9313435c404505626587ef732f481d02da3
YanhuiDua <dyh10280@163.com>
2026-02-02 18:43:27 +0800

    handle last_step_remain_completed_samples in buffer

--- xtuner/v2/proxy_async_env_runner.py
+++ xtuner/v2/proxy_async_env_runner.py
@@ -1,6 +1,6 @@
 import asyncio
 from .simple_env_runner import SimpleEnvRunner
-from .rollout_state import RolloutState
+from .rollout_state import RolloutState, Status
 
 # 用户无感
 class ExpiredBuffer:
@@ -39,20 +39,60 @@ class AbortedBuffer:
         group_staleness = max([rs.staleness for rs in rollout_state]) 
         self.buffer[group_staleness].append(rollout_state)
 
-    def pop(self) -> list[RolloutState]:
+    def pop(self, enable_partial_rollout) -> list[RolloutState]:
         assert self.buffer, "AbortedBuffer is empty!"
         highest_staleness = max(self.buffer.keys())
         rollout_states = self.buffer[highest_staleness]
-        # 拼接是否在这里做呢 ？
-        return rollout_states.pop(0)
+        data = rollout_states.pop(0)
+        if enable_partial_rollout:
+            for rs in data:
+                rs.tokens = rs.prompt_ids + rs.response_ids
+                rs.sample_params.max_tokens = rs.sample_params.max_tokens - len(rs.response_ids)
+        else:
+            for rs in data:
+                rs.response = ""
+                rs.response_ids = []
+                rs.logprobs = []
+                if rs.routed_experts is not None:
+                    del rs.routed_experts
+                rs.routed_experts = None
+                rs.reward = None
+                rs.staleness = 0
+                from .rollout_state import Status
+                rs.state = Status.INIT
+        return data
     
-    def update_staleness(self):
+    def update(self):
         new_buffer = defaultdict(list)
         for staleness, rollout_states in self.buffer.items():
             new_staleness = staleness + 1
             new_buffer[new_staleness].extend(rollout_states)
         self.buffer = new_buffer
 
+class CompletedBuffer:
+    def __init__(self):
+        self.buffer: Dict[int, List[List[RolloutState]]] = defaultdict(list)
+
+    def add(self, rollout_state: List[RolloutState]):
+        group_staleness = max([rs.staleness for rs in rollout_state]) 
+        self.buffer[group_staleness].append(rollout_state)
+    
+    def pop(self) -> list[RolloutState]:
+        highest_staleness = max(self.buffer.keys())
+        rollout_states = self.buffer[highest_staleness]
+        return rollout_states.pop(0)
+
+    def update(self):
+        new_buffer = defaultdict(list)
+        for staleness, rollout_states in self.buffer.items():
+            new_staleness = staleness + 1
+            new_buffer[new_staleness].extend(rollout_states)
+        self.buffer = new_buffer
+
+    @property
+    def length(self) -> int:
+        return sum(len(v) for v in self.buffer.values())
+    
 class Buffer:
     # 这个功能独立的作为一个类的想法是：expired buffer 和 aborted buffer 可能会有不同的优先级管理，
     # 例如，我们现在是根据版本进行管理，版本越旧的样本越先出队，可能未来还有其他的方式，例如按照长度？
@@ -68,6 +108,7 @@ class Buffer:
                  tail_batch_trigger_size: int = 10):
         self.expired_buffer: ExpiredBuffer = ExpiredBuffer()
         self.aborted_buffer: AbortedBuffer = AbortedBuffer()
+        self.completed_buffer: CompletedBuffer = CompletedBuffer()
         self.enable_tail_batch = tail_batch_candidate_step > 0
         self.enable_partial_rollout = enable_partial_rollout
         self.tail_batch_candidate_step = tail_batch_candidate_step
@@ -75,27 +116,37 @@ class Buffer:
     def add(self, rollout_state: List[RolloutState]):
         # rollout_state的版本管理放在哪里？例如一次权重更新后，对Buffer里所有样本的版本进行一次更新，而不是在这里进行更新
         group_staleness = max([rs.staleness for rs in rollout_state])
+        group_states = [rs.state for rs in rollout_state]
         if self.enable_tail_batch and group_staleness > self.tail_batch_candidate_step:
             self.expired_buffer.add(rollout_state)
-        else:
+        elif all(state == Status.COMPLETED for state in group_states):
+            self.completed_buffer.add(rollout_state)
+        else:  
             self.aborted_buffer.add(rollout_state)
 
     def get_sample_func(self, data_sampler: DataSampler) -> RolloutState:
         use_expired = self.enable_tail_batch and len(self.expired_buffer) > 0
-        self._update_staleness()
-        
+        self.update()
+
         def _sample():
             if use_expired and self.expired_buffer:
                 return self.expired_buffer.pop()
             elif self.aborted_buffer:
-                return self.aborted_buffer.pop()
+                return self.aborted_buffer.pop(self.enable_partial_rollout)
             else:
                 return data_sampler.sample_from_dataset()
         
         return _sample 
+    
+    def update(self):
+        if self.enable_partial_rollout:
+            self.completed_buffer.update()
+        else:
+            while self.completed_buffer.length > 0:
+                state = self.completed_buffer.pop()
+                self.aborted_buffer.add(state)
+        self.aborted_buffer.update()
 
-    def update_staleness(self):
-        self.aborted_buffer.update_staleness()
 
 class AsyncProxyEnvRuner:
     def __init__(
@@ -120,11 +171,17 @@ class AsyncProxyEnvRuner:
                                     ) -> List[List[RolloutState]]:
         # 基于当前内部管理的状态，就可以下一次应该从哪个池子中采样
         # 高度内聚功能模块
-        data_concurrency = (1 + self.staleness_threshold) * batch_size
+        last_step_remain_completed_samples = self.buffer.completed_buffer.length
+        data_concurrency = (1 + self.staleness_threshold) * (batch_size - last_step_remain_completed_samples)
         completed_sample_count = 0
         sample_func = self.buffer.get_sample_func(data_sampler, prompt_repeat_k)
 
         pending_tasks = []
+
+        for _ in range(last_step_remain_completed_samples):
+            traj = self.buffer.completed_buffer.pop()
+            yield traj
+
         for _ in range(data_concurrency):
             task = asyncio.create_task(self.base_env_runner.generate_group(sample_func)) 
             # task = asyncio.create_task(self.generate_group(data_sampler.sample(), prompt_repeat_k)) 
@@ -139,8 +196,11 @@ class AsyncProxyEnvRuner:
                 try:
                     traj =  await task
                     if traj is not None:
-                        yield traj
                         completed_sample_count += 1
+                        if completed_sample_count <= batch_size:
+                            yield traj
+                        else:
+                            self.buffer.add(traj)
                 except Exception as e:
                     print(f"Error in generating trajectory: {e}")
         
--- xtuner/v2/rollout_state.py
+++ xtuner/v2/rollout_state.py
@@ -21,17 +21,17 @@ class Status(Enum):
 class RolloutState:
     # dataset 输出必须
     message: list
-    tokens: list[int]
+    tokens: list[int] # 每一次实际输入
     
     uid: int
     session_id: int | None = None
     prompt_ids: list[int]
     response: str
-    response_ids: list[int] 
+    response_ids: list[int] # 每一次实际输出，覆盖写
     logprobs: list[float] 
     routed_experts: list[int] | None = None
     reward: float | list[float] | list[dict] | None = None
-    loss_mask: list[int] | None = None
+    loss_mask: list[int] | None = None # tokens + response_ids的长度
     state: Status = Status.INIT
     sample_parms: SampleParams | None = None
     tools: list | None = None
